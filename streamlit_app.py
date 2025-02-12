6c6609547af0f5568f78912f7eb3b6be621dd017
Anton Schlechter <schlechter@integration-factory.de>
2025-02-12 14:17:47 +0100

    Update streamlit_app.py
    Changes to call and and send REST API calls

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 # Import python packages
+
+import requests
 import streamlit as st
 from snowflake.snowpark.functions import col
 
@@ -44,3 +46,6 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
     
         st.success('Your Smoothie is ordered!', icon="✅")
+
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
